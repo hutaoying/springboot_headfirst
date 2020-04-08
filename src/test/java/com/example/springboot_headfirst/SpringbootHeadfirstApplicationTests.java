483f794573833588c9fbdbe67f351e31288ba2df
hutaoying <tyhu317@163.com>
2020-04-08 10:28:45 +0800

    init

---
+++ src/test/java/com/example/springboot_headfirst/SpringbootHeadfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_headfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHeadfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
